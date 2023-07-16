f493343b46c013ce339922b3f6b9242c314226e6
Elliott Minns <elliott@dreamsofcode.io>
2023-07-16 11:04:59 -0500

    graceful shutdown code added

---
+++ code/006-graceful-shutdown/application/app.go
@@ -0,0 +1,66 @@
+package application
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type App struct {
+	router http.Handler
+	rdb    *redis.Client
+}
+
+func New() *App {
+	app := &App{
+		router: loadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
+	}
+
+	return app
+}
+
+func (a *App) Start(ctx context.Context) error {
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: a.router,
+	}
+
+	err := a.rdb.Ping(ctx).Err()
+	if err != nil {
+		return fmt.Errorf("failed to connect to redis: %w", err)
+	}
+
+  defer func() {
+    if err := a.rdb.Close(); err != nil {
+      fmt.Println("failed to close redis", err)
+    }
+  }()
+
+	fmt.Println("Starting server")
+
+	ch := make(chan error, 1)
+
+	go func() {
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to start server: %w", err)
+		}
+		close(ch)
+	}()
+
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		return server.Shutdown(timeout)
+	}
+
+	return nil
+}
