45e951badf2609e9373d2c5ba3a16f9a2f9abcb0
Elliott Minns <elliott@dreamsofcode.io>
2023-07-16 11:05:22 -0500

    007-data-model added

---
+++ code/007-data-model/model/order.go
@@ -0,0 +1,22 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Order struct {
+	OrderID     uint64     `json:"order_id"`
+	CustomerID  uuid.UUID  `json:"customer_id"`
+	LineItems   []LineItem `json:"line_items"`
+	CreatedAt   *time.Time `json:"created_at"`
+	ShippedAt   *time.Time `json:"shipped_at"`
+	CompletedAt *time.Time `json:"completed_at"`
+}
+
+type LineItem struct {
+	ItemID   uuid.UUID `json:"item_id"`
+	Quantity uint      `json:"quantity"`
+	Price    uint      `json:"price"`
+}
